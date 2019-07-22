36ceaa911e3ece6f24646cb0189d85647212291a
Teng YANG <teng.yang@oocl.com>
2019-07-22 15:08:44 +0800

    Update component & add createTime

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,46 +1,28 @@
 <template>
   <ol class="main-list">
-    <li v-for="item in list" :key="item.id" class="list-item">
-      <div class="item-wrapper">
-        <input type="checkbox"
-               name="toggle-done"
-               v-model="item.finished"
-        >
-        <div contenteditable
-             :class="{'finished': item.finished}"
-             class="todo-item-content"
-             v-text="item.value"
-             @blur="(e) => handleTodoItemValueChange(item, e)"
-             @keypress.enter="handleEnter">
-        </div>
-      </div>
-    </li>
+    <TodoItem v-for="item in list" :key="item.id" :item="item" />
   </ol>
 </template>
 
 <script>
-  export default {
-    name: "TodoItemList",
-    props: {
-      list: {
-        type: Array,
-        default: () => [],
-        require: true
-      }
-    },
-    methods: {
-      handleTodoItemValueChange(item, e) {
-        item.value = e.target.innerText
-      },
-      handleEnter(e) {
-        e.target.blur()
-      }
+import TodoItem from "./TodoItem";
+export default {
+  name: "TodoItemList",
+  components: {
+    TodoItem
+  },
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+      require: true
     }
   }
+};
 </script>
 
 <style>
-  .finished {
-    text-decoration: line-through;
-  }
+.finished {
+  text-decoration: line-through;
+}
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li class="list-item">
+    <div class="item-wrapper">
+      <input type="checkbox" name="toggle-done" v-model="item.finished" />
+      <div
+        contenteditable
+        :class="{'finished': item.finished}"
+        class="todo-item-content"
+        v-text="item.value"
+        @blur="(e) => handleTodoItemValueChange(item, e)"
+        @keypress.enter="handleEnter"
+      ></div>
+      <div class="create-time">{{createDate}}</div>
+    </div>
+  </li>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "TodoItem",
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    createDate() {
+      return moment(this.item.createTime).format("YYYY-MM-DD HH:mm:ss");
+    }
+  },
+  methods: {
+    handleTodoItemValueChange(item, e) {
+      item.value = e.target.innerText;
+    },
+    handleEnter(e) {
+      e.target.blur();
+    }
+  }
+};
+</script>
