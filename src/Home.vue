b8ffa606c9cdd1e59976b499c0d182efa6a13879
Teng YANG <teng.yang@oocl.com>
2019-07-23 16:41:52 +0800

    Add header bar

--- src/Home.vue
+++ src/Home.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="home-wrapper">
-    <div class="side-bar">
-      <ul>
-        <router-link
-          v-for="link in links"
-          :key="link.name"
-          tag="li"
-          :to="link.path"
-          active-class="sidebar-link-active"
-        >
-          <a>{{link.name}}</a>
-        </router-link>
-      </ul>
-    </div>
-    <div class="app-main">
-      <router-view />
+  <div>
+    <header class="home-header">
+      <button @click="onClickBack">Back</button>
+      <div>{{$store.getters.user.username}}</div>
+    </header>
+    <div class="main">
+      <div class="home-wrapper">
+        <div class="side-bar">
+          <ul>
+            <router-link
+              v-for="link in links"
+              :key="link.name"
+              tag="li"
+              :to="link.path"
+              active-class="sidebar-link-active"
+            >
+              <a>{{link.name}}</a>
+            </router-link>
+          </ul>
+        </div>
+        <div class="app-main">
+          <router-view />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +43,15 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    onClickBack() {
+      const res = confirm("Are you sore to go back? ");
+      console.log(res)
+      if (res) {
+        this.$router.push("/");
+      }
+    }
   }
 };
 </script>
@@ -51,6 +68,13 @@ export default {
 }
 .sidebar-link-active a {
   color: #fc999b;
+}
+.home-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.1);
+  height: 60px;
 }
 </style>
 
