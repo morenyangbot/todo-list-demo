182243a0b934945cb87751543af4030a70d876e6
Teng YANG <teng.yang@oocl.com>
2019-07-23 16:09:43 +0800

    Implement home page

--- src/Home.vue
+++ src/Home.vue
@@ -1,12 +1,56 @@
 <template>
-  <div>
-    <router-view />
+  <div class="home-wrapper">
+    <div class="side-bar">
+      <ul>
+        <router-link
+          v-for="link in links"
+          :key="link.name"
+          tag="li"
+          :to="link.path"
+          active-class="sidebar-link-active"
+        >
+          <a>{{link.name}}</a>
+        </router-link>
+      </ul>
+    </div>
+    <div class="app-main">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      links: [
+        {
+          name: "Todo List",
+          path: "/app/todos"
+        },
+        {
+          name: "My Profile",
+          path: "/app/user"
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style>
+.home-wrapper {
+  display: flex;
+}
+.side-bar {
+  width: 360px;
+}
+.app-main {
+  flex: 1;
+}
+.sidebar-link-active a {
+  color: #fc999b;
+}
+</style>
 
