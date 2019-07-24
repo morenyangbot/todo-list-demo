a23522edc5e6c5193d108fd9655844e60b658c8a
Teng YANG <morenyang88@gmail.com>
2019-07-24 20:37:35 +0800

    Update build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+git fetch --all
+git reset --hard origin/async
+
+if [ $NVM_DIR ]
+then echo "Your nvm dir is $NVM_DIR";
+else
+  echo "Can not find nvm on this machine."
+  exit 1
+fi
+
+nvm install
+
+YARN_VERSION=`yarn -v`
+if [ $YARN_VERSION ]
+then echo "Current yarn version $YARN_VERSION"
+else
+  echo "Can not find yarn on this machine."
+  exit 1
+fi
+
+yarn install
+yarn build
+
+rm -r dist
+cp -r dist/* ~/prod/vue-todo-list/
+
+nginx -s reload
+echo "Deploy application successfully"
+
