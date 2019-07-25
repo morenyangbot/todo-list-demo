cfe2aab84da669923b5777fae30e1826447373b8
Teng YANG <morenyang88@gmail.com>
2019-07-25 19:30:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,21 +3,21 @@
 git fetch --all
 git reset --hard origin/async
 
-if [ $NVM_DIR ]
-then echo "Your nvm dir is $NVM_DIR";
-else
-  echo "Can not find nvm on this machine."
-  exit 1
-fi
+# if [ $NVM_DIR ]
+# then echo "Your nvm dir is $NVM_DIR";
+# else
+#  echo "Can not find nvm on this machine."
+#  exit 1
+# fi
 
-nvm install
+# nvm install
 
 YARN_VERSION=`yarn -v`
 if [ $YARN_VERSION ]
 then echo "Current yarn version $YARN_VERSION"
 else
-  echo "Can not find yarn on this machine."
-  exit 1
+  curl -o- -L https://yarnpkg.com/install.sh | bash
+  export PATH="$HOME/.yarn/bin:$PATH"
 fi
 
 rm -r dist
